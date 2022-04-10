c69ae62257bfc9ec02f5bb224cab9066335dfedf
Augie Bren <Augie>
2022-04-10 14:11:12 -0500

    test

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,26 @@
+const { json } = require("express/lib/response");
 
 // Your Code Here
+
+async function getBookList(){
+    let response = await fetch('http://localhost:3001/listbooks')
+    let bookList = 
+}
+
+
+
+
+
+// let response = await fetch('http://localhost:3001/updateBook', {
+//     method: 'PATCH',
+//     headers: {
+//         'Content-Type': 'application/json'
+
+//     },
+//     body: JSON.stringify({
+//         "id": 3,
+//         "title": "Legends Arathrea",
+//     }),
+// })
+// let updateBook = await response.json();
+// console.log(updateBook)
